6d922ef7f630728a608f313f3589cc5cab0f5c01
Jens Bauer <lgw_bauerj@web.de>
2020-05-21 21:55:28 +0100

    small chnages

--- Pratise.py
+++ Pratise.py
@@ -3,11 +3,12 @@
 #for i in range(0, 10):
 #    lis_number.append(ra.random())
 
-lis_number=[5,2,6,3,1,7,12]
+lis_number=[5,2,6,3,1,23,12,6544]
 for i in range(0, len(lis_number)):
     j = i
     while j > 0 and lis_number[j - 1] > lis_number[j]:
-        lis_number[j], lis_number[j - 1] = lis_number[j - 1], lis_number[j]
+        lis_number[j], lis_number[j - 1] = lis_number[j - 1], lis_number[n]+2223
         j -= 1
 
 print(lis_number)
+print(me)
